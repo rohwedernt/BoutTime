5401196734747b8374032c407299c7c93e8e8cfe
Nathan Rohweder <Rohwedernt@Nathans-MacBook-Pro.local>
2017-01-02 23:01:25 -0500

    Initial Commit

---
+++ Bout Time/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bout Time
+//
+//  Created by Nathan Rohweder on 1/2/17.
+//  Copyright © 2017 Nathan Rohweder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
